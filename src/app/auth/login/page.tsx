cf83e327c8ba0c7afb2f88a2fefd988a60dc35b0
Favour Mbata <favourmbta520@gmail.com>
2025-11-01 16:29:48 +0100

    staging

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -8,7 +8,7 @@ const LoginPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center p-6 bg-white">
       <div className="w-full max-w-[1349px] h-auto md:h-[935px] flex flex-col md:flex-row items-stretch overflow-hidden gap-10">
-        {/* Left side - Image section - Hidden on mobile */}
+        
         <div className="hidden md:block md:w-[700px] h-full">
           <Image
             src="/Frame 1.png"
@@ -20,15 +20,14 @@ const LoginPage = () => {
           />
         </div>
 
-        {/* Right side - Form section */}
+      
         <div className="w-full md:w-[609px] h-auto min-h-screen md:min-h-[800px] bg-[#FBFAFC] rounded-[20px] md:rounded-[40px] relative flex flex-col">
          
-          {/* Logo */}
           <div className="pt-8 pl-6 md:pt-[50px] md:pl-[50px]">
             <Image width={55} height={48} src="/Group 1.png" alt="Company Logo" className="object-contain" />
           </div>
 
-          {/* Welcome Text */}
+        
           <div className="px-6 md:px-[50px] pt-8 md:pt-12 w-full">
             <div className="w-full h-auto flex flex-col gap-4">
               <h1 className="font-[family-name:var(--font-monument)] text-[24px] md:text-[30px] font-normal leading-[100%] text-gray-900">
